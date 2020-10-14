a74d63efa634a471ccdb691ee57ef7413c9da390
Bharadwaj14 <tbharadwaj14@gmail.com>
2020-10-14 12:07:56 -0400

    Migrated basic content of Personal Budget to Personal Budget React

---
+++ src/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="bottom">
+        <section className="center">
+            All rights reserved &copy; Bharadwaj Tirunagaru 
+            <a href="https://github.com/bharadwajtirunagaru/personal-budget-react" target="_blank" rel="noopener noreferrer">
+                <img  src="./git_hub_icon.png" alt="Source Code link" />
+            </a>
+        </section>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/Hero/Hero.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <section className="hero">
+      <header role="banner">
+        <h1>Personal Budget</h1>
+        <h2>A personal-budget management app</h2>
+      </header>
+    </section>
+  );
+}
+
+export default Hero;
